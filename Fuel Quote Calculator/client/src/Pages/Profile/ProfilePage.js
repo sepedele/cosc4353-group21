4f92732c40c41ca5585ad4e97ba8f9940a37d634
PrisMuh <117515438+PrisMuh@users.noreply.github.com>
2023-02-24 20:07:31 -0600

    Update ProfilePage.js

--- Fuel Quote Calculator/client/src/Pages/Profile/ProfilePage.js
+++ Fuel Quote Calculator/client/src/Pages/Profile/ProfilePage.js
@@ -1,11 +1,126 @@
-import React from 'react';
+//code for the profile page
 
-function Profile() {
+import React, { useState } from 'react';
+import './ProfilePage.css';
+
+function ProfilePage() {
+  const [fullName, setName] = useState('Giovanni Gonzalez');
+  const [address1, setAddress1] = useState('123 Main St');
+  const [address2, setAddress2] = useState('');
+  const [city, setCity] = useState('Houston');
+  const [state, setState] = useState('Texas');
+  const [zipcode, setZipcode] = useState('12345');
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditMode(true);
+  };
+
+  const handleSaveClick = () => {
+    setIsEditMode(false);
+  };
+
   return (
-    <div>
-      <h1>Your profile</h1>
+
+    <div className="grid-container">
+      <h1>Profile Page</h1>
+
+      <form>
+        <div className="grid-item">
+        <label className="form-label" htmlFor="fullName">Full Name:</label>
+          {isEditMode ? (            //if isEditMode is true, then show the input field
+            <input 
+              className='form-control'
+              type="text" 
+              id="name" 
+              value={fullName} 
+              onChange={(e) => setName(e.target.value)} />
+          ) : (
+            <span>{fullName}</span>   //if isEditMode is false, then show the span
+          )}
+        </div>
+
+        <div className="grid-item">
+        <label className="form-label" htmlFor="address1">Address 1:</label>
+          {isEditMode ? (
+            <input 
+              className='form-control'
+              type="address1" 
+              id="address" 
+              value={address1} 
+              onChange={(e) => setAddress1(e.target.value)} />
+          ) : (
+            <span>{address1}</span>
+          )}
+        </div>
+
+        <div className="grid-item">
+        <label className="form-label" htmlFor="address2">Address 2:</label>
+          {isEditMode ? (
+            <input 
+              className='form-control'
+              type="text" 
+              id="address" 
+              value={address2} 
+              onChange={(e) => setAddress2(e.target.value)} />
+          ) : (
+            <span>{address2}</span>
+          )}
+        </div>
+
+        <div className="grid-item">
+        <label className="form-label" htmlFor="city">City:</label>
+          {isEditMode ? (
+            <input 
+              className='form-control'
+              type="tel" 
+              id="phone" 
+              value={city} 
+              onChange={(e) => setCity(e.target.value)} />
+          ) : (
+            <span>{city}</span>
+          )}
+        </div>
+
+        <div className="grid-item">
+        <label className="form-label" htmlFor="state">State:</label>
+          {isEditMode ? (
+            <input 
+              className='form-control'
+              id="state" 
+              value={state} 
+              onChange={(e) => setState(e.target.value)} />
+          ) : (
+            <span>{state}</span>
+          )}
+
+        </div>
+        <div className="grid-item">
+        <label className="form-label" htmlFor="zipcode">Zipcode:</label>
+          {isEditMode ? (
+            <input 
+              className='form-control'
+              type="text" 
+              id="zipcode"
+              value={zipcode} 
+              onChange={(e) => setZipcode(e.target.value)} />
+          ) : (
+            <span>{zipcode}</span>
+          )}
+
+        </div>
+      </form>
+
+      <div className="form-buttons">
+      {isEditMode ? (
+        <button onClick={handleSaveClick}>Save</button>
+      ) : (
+        <button onClick={handleEditClick}>Edit</button>
+      )}
+      </div>
+
     </div>
   );
 }
 
-export default Profile;
+export default ProfilePage;
