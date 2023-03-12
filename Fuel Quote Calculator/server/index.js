8526340ef60a1272da79c7554c03f1c60dd4d02a
sepedele <sebastianperezdl@gmail.com>
2023-03-11 18:19:50 -0600

    added quote history backend + frontend edits

--- Fuel Quote Calculator/server/index.js
+++ Fuel Quote Calculator/server/index.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const login = require('./Backend_Modules/LoginRegister/Login');
+const login = require("./Backend_Modules/LoginRegister/Login");
 app.use(login);
 
-const register = require('./Backend_Modules/LoginRegister/Register');
+const register = require("./Backend_Modules/LoginRegister/Register");
 app.use(register);
 
-
-
+const fuelquote = require("./Backend_Modules/FuelQuote/FuelQuote");
+app.use(fuelquote);
 
 app.listen(3001, () => {
-    console.log('Server running on port 3001');
+  console.log("Server running on port 3001");
 });
