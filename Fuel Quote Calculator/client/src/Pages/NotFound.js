2674cd44dcd0c52fa25e584f2353f155d2c4294c
lucas <52355519+bartlelu@users.noreply.github.com>
2023-03-24 22:02:04 -0500

    Delete Fuel Quote Calculator directory

--- Fuel Quote Calculator/client/src/Pages/NotFound.js
+++
@@ -1,5 +0,0 @@
-import "./NotFound.css";
-
-export const NotFound = () => {
-    return <h1 className="errorText"> PAGE NOT FOUND </h1>
-};
--- Fuel Quote Calculator/server/Validations/credentialValidation.js
+++
@@ -1,8 +0,0 @@
-const yup = require('yup');
-
-const userSchema = yup.object({
-    username: yup.string().min(4).max(20).required("A username is required"),
-    password: yup.string().min(4).max(20).required(),
-});
-
-module.exports = userSchema;
