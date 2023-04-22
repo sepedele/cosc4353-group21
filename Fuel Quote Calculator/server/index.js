c11bda15fed6245ce0015a8932098e17557f73c9
PrisMuh <117515438+PrisMuh@users.noreply.github.com>
2023-04-22 17:58:34 -0500

    Updated server side index.js
    Updated server side index.js to reflect the Profile Page addition.

--- Fuel Quote Calculator/server/index.js
+++ Fuel Quote Calculator/server/index.js
@@ -10,6 +10,12 @@ app.use(register);
 const fuelquote = require("./Backend_Modules/FuelQuote/FuelQuote");
 app.use(fuelquote);
 
+const profileRegister = require('./Backend_Modules/Profile/ProfileRegister');
+app.use(profileRegister);
+
+const profilePage = require('./Backend_Modules/Profile/ProfilePage');
+app.use(profilePage);
+
 app.listen(3001, () => {
   console.log("Server running on port 3001");
 });
